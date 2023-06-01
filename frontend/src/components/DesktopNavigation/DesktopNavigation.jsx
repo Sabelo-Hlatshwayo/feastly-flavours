b117225e87455ad560c55f3b8ae1304fb3ff925a
Sabelo <sabelo-hlatshwayo>
2023-06-02 00:25:04 +0200

    Added the heading

--- frontend/src/components/DesktopNavigation/DesktopNavigation.jsx
+++ frontend/src/components/DesktopNavigation/DesktopNavigation.jsx
@@ -2,7 +2,9 @@ import "./DesktopNavigation.scss";
 
 function DesktopNavigation() {
   return (
-    <nav className="desktopNavigation">THIS IS THE DESKTOP NAVIGATION</nav>
+    <nav className="desktopNavigation">
+      <h1 className="desktopNavigation__heading">feastly flavours</h1>
+    </nav>
   );
 }
 
