d8788e565efc338c9885ca3df2ae9a49e0cada23
Sabelo <sabelo-hlatshwayo>
2023-06-01 19:57:34 +0200

    Added the initial configuration of the Remix router instance

---
+++ frontend/src/router.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>FEASTLY FALVOURS</h1>,
+  },
+]);
+
+export default router;
