97d16336bc23743b1b3ab86240205189163d781e
Sabelo <sabelo-hlatshwayo>
2023-06-01 20:58:32 +0200

    Added the module for the Showcase component and updated the configuration of the Remix router instance

---
+++ frontend/src/components/Showcase/Showcase.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Showcase() {
+  return <main>THIS IS THE SHOWCASE</main>;
+}
+
+export default Showcase;
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout/RootLayout";
+import Showcase from "./components/Showcase/Showcase";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Showcase />,
+      },
+    ],
   },
 ]);
 
