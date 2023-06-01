93f337c32100ecc895b736bdd37e95469ce9b7e1
Sabelo <sabelo-hlatshwayo>
2023-06-02 01:12:24 +0200

    Added the desktop navigation's list

--- frontend/src/components/DesktopNavigation/DesktopNavigation.jsx
+++ frontend/src/components/DesktopNavigation/DesktopNavigation.jsx
@@ -1,9 +1,25 @@
+import { NavLink } from "react-router-dom";
+import { Home, Heart } from "lucide-react";
 import "./DesktopNavigation.scss";
 
 function DesktopNavigation() {
   return (
     <nav className="desktopNavigation">
       <h1 className="desktopNavigation__heading">feastly flavours</h1>
+      <ul className="desktopNavigation__list">
+        <li className="desktopNavigation__item">
+          <NavLink className="desktopNavigation__link">
+            <Home />
+            <span>home</span>
+          </NavLink>
+        </li>
+        <li className="desktopNavigation__item">
+          <NavLink className="desktopNavigation__link">
+            <Heart />
+            <span>favourites</span>
+          </NavLink>
+        </li>
+      </ul>
     </nav>
   );
 }
