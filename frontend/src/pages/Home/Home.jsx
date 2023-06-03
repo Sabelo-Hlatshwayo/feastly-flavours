95f165553c4cc12ba094837c6e99d6f5f7bf13ec
Sabelo <sabelo-hlatshwayo>
2023-06-03 23:28:18 +0200

    Added the search form's markup

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
 import "./Home.scss";
+import { Search } from "lucide-react";
 
 function Home() {
-  return <main className="home">THIS IS THE SHOWCASE</main>;
+  return (
+    <main className="home">
+      <form className="searchForm">
+        <input
+          type="text"
+          placeholder="What do you want to cook today?"
+          className="searchForm__input"
+        />
+        <button type="submit" className="searchForm__button">
+          <Search />
+        </button>
+      </form>
+    </main>
+  );
 }
 
 export default Home;
