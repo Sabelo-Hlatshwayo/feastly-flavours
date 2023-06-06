bfaa649c004f6c159b6f2e8c52e41353f224fd1c
Sabelo <sabelo-hlatshwayo>
2023-06-06 07:05:40 +0200

    Added the markup for the MobileNavigation.jsx component

--- frontend/src/components/MobileNavigation/MobileNavigation.jsx
+++ frontend/src/components/MobileNavigation/MobileNavigation.jsx
@@ -1,7 +1,24 @@
+import { NavLink } from "react-router-dom";
+import { Home, Heart } from "lucide-react";
 import "./MobileNavigation.scss";
 
 function MobileNavigation() {
-  return <div className="mobileNavigation">THIS IS THE MOBILE NAVIGATION</div>;
+  return (
+    <nav className="mobileNavigation">
+      <ul className="mobileNavigation__list">
+        <li className="mobileNavigation__item">
+          <NavLink className="mobileNavigation__link">
+            <Home />
+          </NavLink>
+        </li>
+        <li className="mobileNavigation__item">
+          <NavLink className="mobileNavigation__link">
+            <Heart />
+          </NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default MobileNavigation;
