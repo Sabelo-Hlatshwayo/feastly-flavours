9ff90df1c850c9cd9e372f69f8aa454289e2d0e5
Sabelo <sabelo-hlatshwayo>
2023-06-06 06:58:26 +0200

    Added the MobileNavigation.jsx component

---
+++ frontend/src/components/MobileNavigation/MobileNavigation.jsx
@@ -0,0 +1,7 @@
+import "./MobileNavigation.scss";
+
+function MobileNavigation() {
+  return <div className="mobileNavigation">THIS IS THE MOBILE NAVIGATION</div>;
+}
+
+export default MobileNavigation;
--- frontend/src/layouts/RootLayout/RootLayout.jsx
+++ frontend/src/layouts/RootLayout/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router-dom";
 import DesktopNavigation from "../../components/DesktopNavigation/DesktopNavigation";
+import MobileNavigation from "../../components/MobileNavigation/MobileNavigation";
 import "./RootLayout.scss";
 
 function RootLayout() {
   return (
     <div className="rootLayout">
       <DesktopNavigation />
+      <MobileNavigation />
       <Outlet />
     </div>
   );
