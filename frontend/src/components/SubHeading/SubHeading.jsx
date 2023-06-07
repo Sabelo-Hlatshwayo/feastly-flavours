7c0fd66c43f23b24e93312cd0da496d85e174798
Sabelo <sabelo-hlatshwayo>
2023-06-08 01:03:26 +0200

    Added the definition of the SubHeading.jsx component

---
+++ frontend/src/components/SubHeading/SubHeading.jsx
@@ -0,0 +1,7 @@
+import "./SubHeading.scss";
+
+function SubHeading({ title }) {
+  return <h4 className="subHeading">{title}</h4>;
+}
+
+export default SubHeading;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,6 +1,7 @@
 import SearchForm from "../../components/SearchForm/SearchForm";
 import MainContainer from "../../Containers/MainContainer/MainContainer";
 import PrimaryHeading from "../../components/PrimaryHeading/PrimaryHeading";
+import SubHeading from "../../components/SubHeading/SubHeading";
 import "./Home.scss";
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
       <MainContainer>
         <SearchForm />
         <PrimaryHeading title="recommended recipes" />
+        <SubHeading title="popular choices" />
       </MainContainer>
     </main>
   );
