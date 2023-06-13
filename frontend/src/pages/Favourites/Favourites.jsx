f860626d36439770703560596a9b08d4e0698deb
Sabelo <sabelo-hlatshwayo>
2023-06-13 23:54:05 +0200

    Rendered MainContainer.jsx and PrimaryHeading.jsx in Favourites.jsx

--- frontend/src/pages/Favourites/Favourites.jsx
+++ frontend/src/pages/Favourites/Favourites.jsx
@@ -1,7 +1,15 @@
+import MainContainer from "../../Containers/MainContainer/MainContainer";
+import PrimaryHeading from "../../components/PrimaryHeading/PrimaryHeading";
 import "./Favourites.scss";
 
 function Favourites() {
-  return <main className="favourites"></main>;
+  return (
+    <main className="favourites">
+      <MainContainer>
+        <PrimaryHeading title="my favourites" />
+      </MainContainer>
+    </main>
+  );
 }
 
 export default Favourites;
