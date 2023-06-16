934c79dd901876a8813f907c5398e3207ee71dfe
Sabelo <sabelo-hlatshwayo>
2023-06-16 16:10:45 +0200

    Added the constants.js module

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,14 @@
+export const COLORS = {
+  green: {
+    bg: "#ECF7D4",
+    badge: "#D6F497",
+  },
+  orange: {
+    bg: "#F9EFE1",
+    badge: "#F7E0B8",
+  },
+  red: {
+    bg: "#FBE5E7",
+    badge: "#FDC6C7",
+  },
+};
