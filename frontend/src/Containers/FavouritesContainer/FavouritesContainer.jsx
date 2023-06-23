b63cd206d84f0c77018546db4d75b210465c80ae
Sabelo <sabelo-hlatshwayo>
2023-06-23 08:44:50 +0200

    Added the logic for rendering the list of favourite recipes in the recipes page

--- frontend/src/Containers/FavouritesContainer/FavouritesContainer.jsx
+++ frontend/src/Containers/FavouritesContainer/FavouritesContainer.jsx
@@ -1,32 +1,26 @@
-import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { useRecipes } from "../../context/features/RecipesContextProvider";
 import Recipe from "../../components/Recipe/Recipe";
 import "./FavouritesContainer.scss";
 
 function FavouritesContainer() {
-  const [favourites, setfavourites] = useState([]);
+  // const [favourites, setfavourites] = useState([]);
+  const { favourites } = useRecipes();
 
-  // const favouritesList = favourites.map((favourite) => {
-  //   return (
-  //     <Recipe
-  //       key={uuidv4()}
-  //       image={favourite.image}
-  //       label={favourite.label}
-  //       cuisineType={favourite.cuisineType}
-  //       healthLabels={favourite.healthLabels}
-  //       servings={favourite.servings}
-  //     />
-  //   );
-  // });
+  const favouritesList = favourites.map((favourite) => {
+    return (
+      <Recipe
+        key={uuidv4()}
+        image={favourite.image}
+        label={favourite.label}
+        cuisineType={favourite.cuisineType}
+        healthLabels={favourite.healthLabels}
+        servings={favourite.servings}
+      />
+    );
+  });
 
-  // useEffect(() => {
-  //   const favs = JSON.parse(localStorage.getItem("favourites"));
-  //   setfavourites(favs);
-  // }, []);
-
-  // return <div className="favouritesContainer">{favouritesList}</div>;
-  return <div>sabelo</div>;
+  return <div className="favouritesContainer">{favouritesList}</div>;
 }
 
 export default FavouritesContainer;
